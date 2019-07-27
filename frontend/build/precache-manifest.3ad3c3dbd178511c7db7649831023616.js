d674f943ad854c1a326faa6155d5e3db2b38a1b0
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2019-07-26 20:15:10 -0500

    only build

---
+++ frontend/build/precache-manifest.3ad3c3dbd178511c7db7649831023616.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de741679aefec9853d781d21db13bc35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81247d3cb8d00223ba1d",
+    "url": "/static/css/main.1dc96516.chunk.css"
+  },
+  {
+    "revision": "c2eb5e3f85a73b6399eb",
+    "url": "/static/js/2.c67eac96.chunk.js"
+  },
+  {
+    "revision": "81247d3cb8d00223ba1d",
+    "url": "/static/js/main.3a01bd3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
